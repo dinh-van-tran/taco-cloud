24e9d655c25d137dc86a22d27bd7ab9e6b53c0aa
Dinh Tran <tranvan538@gmail.com>
2025-08-19 13:51:33 +0700

    Add custom JPA query.

--- src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -1,7 +1,21 @@
 package com.example.tacocloud.data;
 
 import com.example.tacocloud.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryNameIgnoreCaseAndDeliveryCityIgnoreCase(String deliveryName, String deliveryCity);
+
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryName(String deliveryCity);
+
+    @Query("SELECT o from TacoOrder o where o.deliveryCity = 'Seattle'")
+    List<TacoOrder> findByDeliveryInSeattle();
 }
