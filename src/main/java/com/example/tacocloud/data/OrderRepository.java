9e263b09f6d0d2ab870a419cae4a2ff713e667e6
Dinh Tran <tranvan538@gmail.com>
2025-08-21 14:48:50 +0700

    Add rest api - adding rest starter package. - Change path tacoes to tacoes. - Change default api-path. - Allow sort and paging. - Prevent high-risk api endpoints.

--- src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.tacocloud.data;
 import com.example.tacocloud.TacoOrder;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Date;
 import java.util.List;
@@ -18,4 +19,24 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     @Query("SELECT o from TacoOrder o where o.deliveryCity = 'Seattle'")
     List<TacoOrder> findByDeliveryInSeattle();
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TacoOrder entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Long> longs);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TacoOrder> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
