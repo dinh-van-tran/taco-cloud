1fd8d89593ffa86b6f982c69b6e7728c1490b5b0
Dinh Tran <tranvan538@gmail.com>
2025-08-21 14:29:04 +0700

    Add rest api - adding rest starter package. - Change path tacoes to tacoes. - Change default api-path. - Allow sort and paging.

--- src/main/java/com/example/tacocloud/data/IngredientRepository.java
+++ src/main/java/com/example/tacocloud/data/IngredientRepository.java
@@ -1,7 +1,7 @@
 package com.example.tacocloud.data;
 
 import com.example.tacocloud.Ingredient;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+public interface IngredientRepository extends JpaRepository<Ingredient, String> {
 }
